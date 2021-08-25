90ad44588d786041eb149f7d0d939ad27cf9f47d
Niksutus <niko.kananenfi@gmail.com>
2021-08-25 12:19:20 +0300

    muutokset tehty tehtävän 3.4 mukaisesti

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
