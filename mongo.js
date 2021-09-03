3b80f5152297c4aa1f835eb1f206e059fbd6abdc
Niksutus <niko.kananenfi@gmail.com>
2021-09-03 12:04:52 +0300

    muutokset tehty tehtävän 3.12 mukaisesti

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.rp581.mongodb.net/puhelinluettelosovellus?retryWrites=true&w=majority`
+
+
+if(process.argv.length <3) {
+    console.log('give password as a argument')
+    process.exit(1)
+} 
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(!process.argv[3]){
+    Person.find({}).then(persons => {
+        persons.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    
+    person.save().then(response => {
+        console.log(`added ${process.argv[3]} number ${process.argv[4]}`)
+        mongoose.connection.close()
+    })
+}
+
+
