be90796828e29130715405607f6ea7ddfec6199b
Niksutus <niko.kananenfi@gmail.com>
2021-09-07 16:06:16 +0300

    muutokset tehty tehtävien 3.16 ja 3.18 mukaisesti. Tehtävän 3.17 mukaiset muutokset vielä tekemättä

--- index.js
+++ index.js
@@ -17,31 +17,33 @@ app.use(express.static('build'))
 app.use(morgan('tiny'))
 
 
-let persons = [
-      {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-      },
-      {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-      },
-      {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-      },
-      {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-      }
-    ]
+// let persons = [
+//       {
+//         name: "Arto Hellas",
+//         number: "040-123456",
+//         id: 1
+//       },
+//       {
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523",
+//         id: 2
+//       },
+//       {
+//         name: "Dan Abramov",
+//         number: "12-43-234345",
+//         id: 3
+//       },
+//       {
+//         name: "Mary Poppendieck",
+//         number: "39-23-6423122",
+//         id: 4
+//       }
+//     ]
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people </p><p>${new Date()}</p>`)
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people </p><p>${new Date()}</p>`) 
+    })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -50,22 +52,24 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person){
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
    Person.findByIdAndRemove(req.params.id)
     .then(result => {
         res.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -86,6 +90,18 @@ app.post('/api/persons', (req, res) => {
     })
 
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
